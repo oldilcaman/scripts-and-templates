f5533c74eeecf315f0b75193572a302e53f2c4e9
oldilcaman <peter.liljenroth@tietoevry.com>
2025-08-22 10:58:11 +0200

    trying clean

--- azure/azure-services/vm-scale-sets/infra-test/custom-script-extension/deploy-servernameapi.ps1
+++ azure/azure-services/vm-scale-sets/infra-test/custom-script-extension/deploy-servernameapi.ps1
@@ -75,32 +75,27 @@ try {
 }
 
 
-# try {
-    
-#     # Download published ServerNameAPI zip from Azure Storage or GitHub
-#     Invoke-WebRequest -Uri "https://github.com/peterlil/script-and-templates/releases/download/vmss-scale-sets-ServerNameApi-v0.1/ServerNameApi-v0.1.zip" -OutFile "C:\ServerNameApi.zip"
-#     Expand-Archive -Path "C:\ServerNameApi.zip" -DestinationPath "C:\ServerNameApi" -Force
-
-#     # Start the API (as a background process)
-#     $proc = Start-Process "C:\Program Files\dotnet\dotnet.exe" -ArgumentList "C:\ServerNameApi\ServerNameApi.dll" -WindowStyle Hidden -PassThru
-#     $proc.Id | Out-File "C:\ServerNameApi\dotnet.pid"
-
-#     # Optional: log that the process was started
-#     Write-Output "ServerNameApi started, waiting for initialization..."
-
-#     # Add a short delay to ensure the process has time to spin up
-#     Start-Sleep -Seconds 10
-
-#     $proc = Get-Process -Name "dotnet" -ErrorAction SilentlyContinue
-#     if ($proc) {
-#         Write-Output "dotnet process is running."
-#     } else {
-#         Write-Error "dotnet process failed to start."
-#     }
-
-# } catch {
-#     Write-Error ("Script failed: " + $_)
-# }
+try {
+  
+    # Download published ServerNameAPI zip from Azure Storage or GitHub
+    Invoke-WebRequest -Uri "https://github.com/peterlil/script-and-templates/releases/download/vmss-scale-sets-ServerNameApi-v0.1/ServerNameApi-v0.1.zip" -OutFile "C:\ServerNameApi.zip"
+    Expand-Archive -Path "C:\ServerNameApi.zip" -DestinationPath "C:\ServerNameApi" -Force
+    # Start the API (as a background process)
+    $proc = Start-Process "C:\Program Files\dotnet\dotnet.exe" -ArgumentList "C:\ServerNameApi\ServerNameApi.dll" -WindowStyle Hidden -PassThru
+    $proc.Id | Out-File "C:\ServerNameApi\dotnet.pid"
+    # Optional: log that the process was started
+    Write-Output "ServerNameApi started, waiting for initialization..."
+    # Add a short delay to ensure the process has time to spin up
+    Start-Sleep -Seconds 10
+    $proc = Get-Process -Name "dotnet" -ErrorAction SilentlyContinue
+    if ($proc) {
+        Write-Output "dotnet process is running."
+    } else {
+        Write-Error "dotnet process failed to start."
+    }
+} catch {
+    Write-Error ("Script failed: " + $_)
+}
 
 
 Stop-Transcript
